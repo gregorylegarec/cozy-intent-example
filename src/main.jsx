750410933052ae4d999f590d421bd8b36c159be3
Grégory Le Garec <gregory.legarec@gmail.com>
2017-04-12 21:15:53 +0200

    fix: add cozy-client initialization :ambulance:

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/* global cozy */
 import 'babel-polyfill'
 
 import './styles/main'
@@ -12,9 +13,16 @@ const context = window.context
 const lang = document.documentElement.getAttribute('lang') || 'en'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const applicationElement = document.querySelector('[role=application]')
+
+  cozy.client.init({
+    cozyURL: `//${applicationElement.dataset.cozyDomain}`,
+    token: applicationElement.dataset.cozyToken
+  })
+
   render((
     <I18n context={context} lang={lang}>
       <App />
     </I18n>
-  ), document.querySelector('[role=application]'))
+  ), applicationElement)
 })
